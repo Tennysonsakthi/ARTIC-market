2802d47c931136d62e19a3f569a0979bc8989795
Tennysonsakthi <147335535+Tennysonsakthi@users.noreply.github.com>
2024-06-09 12:21:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function toggle sidebar() {
-    var sidebar = document.getElementById("sidebar");
-    if (sidebar.style.width === "250px") {
-        sidebar. style.width = "0";
-    } else {
-        sidebar. style.width = "250px";
-    }
-}
+function toggleSidebar() {
+            var sidebar = document.getElementById("sidebar");
+            if (sidebar.style.width === "250px") {
+                sidebar.style.width = "0";
+            } else {
+                sidebar.style.width = "250px";
+            }
+        }
